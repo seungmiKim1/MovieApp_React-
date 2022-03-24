5faa11a0344e3a8cef593028c04d0d065e9b4871
brandy <89903766+seungmiKim1@users.noreply.github.com>
2022-03-24 23:25:56 +0900

    220324_React_movie_app 컴포넌트 개념 알아보기

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import './App.css';
+
+function Thursday() { 
+  return <h1>Today is Thursday!</h1>;
+}
+
+function App() {
+  return(
+    /*<div className="App"/>;*/
+    <div>
+      
+      <h1>I'm tired!</h1>
+      <Thursday />
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <App />, // App() 컴포넌트를 import 해옴 
+  document.getElementById('root')
+);
+
+reportWebVitals();
