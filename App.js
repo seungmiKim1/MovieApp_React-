c507a8bbfb35386a7ba5459cd32022518b4477a9
brandy <89903766+seungmiKim1@users.noreply.github.com>
2022-04-06 11:48:30 +0900

    0406_map()함수에 key props 추가

--- App.js
+++ App.js
@@ -13,45 +13,54 @@ function Food({name,picture}) {
   return (
     <div>
   <h2>I like {name}</h2>
-  <img src={picture}/>
+  <img src={picture} alt={name}/>
   </div>
   );
   }
 
 const foodILike = [
   {
+    id : 1, 
     name : 'Kimchi',
     image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcbmpress.sfo2.digitaloceanspaces.com%2Ftfood%2F2516102861_WtlGqsyD_6a53a2f10d7bcaf273d900e63e1ecb90dbd28318.jpg&f=1&nofb=1', 
   },
   { 
+    id : 2, 
     name : 'Samgyeopsal',
     image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR800x0%2F%3Fscode%3Dmtistory2%26fname%3Dhttps%3A%252F%252Fblog.kakaocdn.net%252Fdn%252FdDyTJN%252FbtqvrVnOCb4%252FVDVFUo7oXVnoFqlVIzH0h0%252Fimg.png&f=1&nofb=1'
   },
 
   { 
+    id : 3,
     name : 'Ramyeon',
     image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR800x0%2F%3Fscode%3Dmtistory2%26fname%3Dhttps%3A%252F%252Ft1.daumcdn.net%252Fcfile%252Ftistory%252F990C324C5D9EDFBD0E&f=1&nofb=1'
   },
 
-  { 
+  {  
+    id : 4,
     name : 'Udon',
     image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR800x0%2F%3Fscode%3Dmtistory2%26fname%3Dhttps%3A%252F%252Ft1.daumcdn.net%252Fcfile%252Ftistory%252F990C324C5D9EDFBD0E&f=1&nofb=1'
   },
 
   { 
+    id : 5,
     name : 'Gimbap',
     image : 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Frecipe.ezmember.co.kr%2Fcache%2Frecipe%2F2017%2F04%2F19%2Fd20b047290d4e88e20c451798c0b3ec11.jpg&f=1&nofb=1'
   },
 ]; //foodILike 변수에 빈 배열을 생성 
 
-
+/*function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image}/>;
+}
+// const renderFood = dish => <Food name={dish.nam} picture={dish.image}/>;도 같은 표현이다. */
 
 function App() {
+  //console.log(foodILike.map(renderFood));
   return(
     /*<div className="App"/>;*/
     <div>
-      {foodILike.map(dish=>(<Food name={dish.name} picture={dish.image}/>))} {/*위에서 food라는 컴포넌트를 만들고 여기서 사용*/}
-      
+      {foodILike.map(dish=>(<Food key={dish.id} name={dish.name} picture={dish.image}/>))} {/*위에서 food라는 컴포넌트를 만들고 여기서 사용*/}
+      {/*{foodILike.map(renderFood)}*/} 
       
     </div>
   );
