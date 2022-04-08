17274c88ff3dc4cf12f5f45305035f0e8770a879
brandy <89903766+seungmiKim1@users.noreply.github.com>
2022-04-08 16:08:01 +0900

    220408_리액트생명주기

--- App.js
+++ App.js
@@ -8,23 +8,57 @@ class App extends React.Component {
   // 그래서 함수 형태의 App 컴포넌트처럼 JSX를 반환할 수 없다. 
   // 클래스형 컴포넌트에서는 JSX를 반환하기 위해 render() 함수 사용
 
- 
+    constructor(props) {
+      super(props);
+      console.log('hello');
+    }
     state ={ 
       count: 0, // count를 key값으로 넣는다. 객체 형태니깐. 
 
     };
 
-    add = () => {
+    /*add = () => {
      // console.log('add'); 
-     this.setState({count:1});
-        };
+     this.setState({count:this.state.count+1});
+        }; */
 
-   minus = () => {
-      //console.log('minus'); 
-      this.setState({count:-1});
+    // current에는 현재의 state가 넘어온다. 새로운 state로 교체되는 것이 아닌 state를 비교하여 바뀐 데이터만 업데이트한다.
+    // 그래서 변경 대상이 아닌 키와 키값은 그대로 유지된다.
+    add = () => { 
+      this.setState(current => ({
+        count : current.count+1,
+      }));
     };
 
+    minus = () => {
+      this.setState(current => ({
+        count : current.count-1,
+      }));
+    };
+
+  /* minus = () => {
+      //console.log('minus'); 
+      this.setState({count:this.state.count-1});
+    }; */
+
+    componentDidMount() {
+      console.log('component rendered');
+    }
+
+    componentDidUpdate() {
+      console.log('I just updated');
+    }
+
+    componentWillUnmount() {
+      console.log('Goodbye, cruel world');
+    }
+
+
+
+
     render() {
+      console.log("I'm rendering");
+
     return(
       <div>
         <h1>The number is: {this.state.count} </h1>
